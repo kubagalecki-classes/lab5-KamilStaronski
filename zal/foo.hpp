04c512f8357686e6705114e1c53945d928ba6b33
KamilStaronski <73498649+KamilStaronski@users.noreply.github.com>
2020-12-09 08:51:09 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+  std::vector< char > v1;
+  for(Human elem : people){
+    elem.birthday();
+  }
+  std::list<Human> ::reverse_iterator iterR;
+  for(iterR=people.rbegin(); iterR!=people.rend();++iterR){
+    if((*iterR).isMonster()==1){
+      v1.push_back('n');
+    }
+    else{
+      v1.push_back('y');
+    }
+  }
+  return v1;
 }
